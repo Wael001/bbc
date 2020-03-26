44a621663ec914913ba9e31fe4f5834c28b63c14
Glitch (hello-express) <none>
2020-03-26 08:50:33 +0000

    📊🏑 Checkpoint ./config.js:8093589/227 ./package.json:8093589/2843 ./server.js:8093589/15286

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NjQ3MTAzNzEyOTkyNjkwMjE3.Xnxp_A.7kEK6ibHeNbBXM9636pvv_PvKas",
+  YT_API_KEY: "AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8",
+  prefix: "!",
+  devs: ["521783741576642560"]
+};
--- server.js
+++ server.js
@@ -1,34 +1,140 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const http = require("http");
 const express = require("express");
 const app = express();
+var adminprefix = "!";
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`https://bcgc1231.glitch.me/`);
+}, 280000);
+
+// كل البكجات الي ممكن تحتجها في اي بوت
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require("discord.js");
+const { TOKEN, YT_API_KEY, prefix, devs } = require("./config");
+const client = new Client({ disableEveryone: true });
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const Canvas = require("canvas");
+const convert = require("hh-mm-ss");
+const fetchVideoInfo = require("youtube-info");
+const botversion = require("./package.json").version;
+const simpleytapi = require("simple-youtube-api");
+const moment = require("moment");
+const fs = require("fs");
+const util = require("util");
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require("snekfetch");
+const guild = require("guild");
+const dateFormat = require("dateformat"); //npm i dateformat
+const YouTube = require("simple-youtube-api");
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const hastebins = require("hastebin-gen");
+const getYoutubeID = require("get-youtube-id");
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const pretty = require("pretty-ms");
+client.login(TOKEN);
+const queue = new Map();
+var table = require("table").table;
+const Discord = require("discord.js");
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+const developers = ["521783741576642560", "505188754168872960"];
+client.on("message", message => {
+  var argresult = message.content
+    .split(` `)
+    .slice(1)
+    .join(" ");
+  if (!developers.includes(message.author.id)) return;
+
+  if (message.content.startsWith(adminprefix + "setg")) {
+    client.user.setGame(argresult);
+    message.channel.send(`**✅   ${argresult}**`);
+  } else if (message.content === adminprefix + "leave") {
+    message.guild.leave();
+  } else if (message.content.startsWith(adminprefix + "setw")) {
+    client.user.setActivity(argresult, { type: "WATCHING" });
+    message.channel.send(`**✅   ${argresult}**`);
+  } else if (message.content.startsWith(adminprefix + "setl")) {
+    client.user.setActivity(argresult, { type: "LISTENING" });
+    message.channel.send(`**✅   ${argresult}**`);
+  } else if (message.content.startsWith(adminprefix + "sets")) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+    message.channel.send(`**✅**`);
+  }
+  if (message.content.startsWith(adminprefix + "setname")) {
+    client.user.setUsername(argresult).then;
+    message.channel.send(`Changing The Name To ..**${argresult}** `);
+  } else if (message.content.startsWith(adminprefix + "setava")) {
+    client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+  }
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+client.on("message", message => {
+  if (message.content.startsWith("-bc")) {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(" ");
+    message.guild.members
+      .filter(m => m.presence.status !== "all")
+      .forEach(m => {
+        m.send(`${argresult}\n ${m}`);
+      });
+    message.channel.send(
+      `\`${
+        message.guild.members.filter(m => m.presence.status !== "all").size
+      }\`:mailbox:  عدد المستلمين `
+    );
+    message.delete();
+  }
 });
+
+client.on("message", async message => {
+  if (message.content.startsWith(prefix + "help")) {
+    let help = new Discord.RichEmbed().setColor("0x5016f3")
+      .setDescription(`** ❤❤ By US OF GC ❤❤
+                الاوامر  ❤❤&❤❤  Command
+             ✅ برودكاست عادي : ${prefix}bc 🔴
+         =============================
+            📛 **SUPPORT_Bot_US_OF_GC** 📛
+             📛 https://discord.gg/8NPXPej 📛
+              **`);
+    message.channel.sendEmbed(help);
+  }
+});
+
+client.on("message", function(message) {
+  let prefix = "!";
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (message.content.startsWith(prefix + "say")) {
+    if (!args) return;
+    message.channel.send(`** ${args}**`);
+  }
+});
+
+console.log("==================================");
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("=========> Bot Online <=========");
+console.log("========> TestBot <========");
+console.log("=======> Token Bot **** <=======");
+console.log("3");
+console.log("2");
+console.log("1");
+console.log("====================================");
+console.log("Bot Online 24/7");
+///
+//الاكواد
