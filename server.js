fc34bb5b0bae176471f7f5f580f3ad07e6108a39
Glitch (hello-express) <none>
2020-03-27 11:13:58 +0000

    👭💇 Checkpoint ./server.js:8093589/38318

--- server.js
+++ server.js
@@ -79,22 +79,14 @@ client.on("message", message => {
   }
 });
 
-client.on("message", message => {
-  if (message.content.startsWith("!bc")) {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    let args = message.content.split(" ").slice(1);
-    var argresult = args.join(" ");
-    message.guild.members
-      .filter(m => m.presence.status !== "all")
-      .forEach(m => {
-        m.send(`${argresult}\n ${m}`);
-      });
-    message.channel.send(
-      `\`${
-        message.guild.members.filter(m => m.presence.status !== "all").size
-      }\`:mailbox:  عدد المستلمين `
-    );
-    message.delete();
+client.on("message", async message => {
+  if (message.content.startsWith(prefix + "banned")) {
+    if (!message.guild) return;
+    message.guild.fetchBans().then(bans => {
+      let b = bans.size;
+      let bb = bans.map(a => `${a}`).join(" - ");
+      message.channel.send(`**\`${b}\` | ${bb}**`);
+    });
   }
 });
 
@@ -124,119 +116,148 @@ client.on("message", function(message) {
   }
 });
 
-
 client.on("message", async message => {
-    var command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-        if(!message.channel.guild) return;
-            var args = message.content.split(" ").slice(1).join(" ");
-            if(command == "bc") {
-                if(!message.member.hasPermission("ADMINISTRATOR")) {
-                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
-                }
-                    if(!args) {
-                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
-                    }
-                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
-                            m.react("✅")
-                            .then(() => m.react("❌"));
+  var command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  if (!message.channel.guild) return;
+  var args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (command == "bc") {
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
+      return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+    }
+    if (!args) {
+      return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+    }
+    message.channel
+      .send(
+        `**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`
+      )
+      .then(m => {
+        m.react("✅").then(() => m.react("❌"));
 
-                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
-                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+        let yesFilter = (reaction, user) =>
+          reaction.emoji.name == "✅" && user.id == message.author.id;
+        let noFiler = (reaction, user) =>
+          reaction.emoji.name == "❌" && user.id == message.author.id;
 
-                            let yes = m.createReactionCollector(yesFilter);
-                            let no = m.createReactionCollector(noFiler);
+        let yes = m.createReactionCollector(yesFilter);
+        let no = m.createReactionCollector(noFiler);
 
-                            yes.on("collect", v => {
-                                m.delete();
-                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.memberCount} Members`).then(msg => msg.delete(5000));
-                                        message.guild.members.forEach(member => {
-                                            let bc = new Discord.RichEmbed()
-                                            .setColor("RANDOM")
-                                            .setThumbnail(message.author.avatarURL)
-                                            .setTitle("Broadcast")
-                                            .addField("Server", message.guild.name)
-                                            .addField("Sender", message.author.username)
-                                            .addField("Message", args);
+        yes.on("collect", v => {
+          m.delete();
+          message.channel
+            .send(
+              `:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.memberCount} Members`
+            )
+            .then(msg => msg.delete(5000));
+          message.guild.members.forEach(member => {
+            let bc = new Discord.RichEmbed()
+              .setColor("RANDOM")
+              .setThumbnail(message.author.avatarURL)
+              .setTitle("Broadcast")
+              .addField("Server", message.guild.name)
+              .addField("Sender", message.author.username)
+              .addField("Message", args);
 
-                                            member.sendEmbed(bc);
-                                        });
-                        });
-                        no.on("collect", v => {
-                            m.delete();
-                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000));
-                        });
-                            
-                        });
-            }
-            if(command == "bco") {
-                if(!message.member.hasPermission("ADMINISTRATOR")) {
-                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
-                }
-                    if(!args) {
-                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
-                    }
-                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
-                            m.react("✅")
-                            .then(() => m.react("❌"));
+            member.sendEmbed(bc);
+          });
+        });
+        no.on("collect", v => {
+          m.delete();
+          message.channel
+            .send("**Broadcast Canceled.**")
+            .then(msg => msg.delete(3000));
+        });
+      });
+  }
+  if (command == "bco") {
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
+      return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+    }
+    if (!args) {
+      return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+    }
+    message.channel
+      .send(
+        `**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`
+      )
+      .then(m => {
+        m.react("✅").then(() => m.react("❌"));
 
-                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
-                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+        let yesFilter = (reaction, user) =>
+          reaction.emoji.name == "✅" && user.id == message.author.id;
+        let noFiler = (reaction, user) =>
+          reaction.emoji.name == "❌" && user.id == message.author.id;
 
-                            let yes = m.createReactionCollector(yesFilter);
-                            let no = m.createReactionCollector(noFiler);
+        let yes = m.createReactionCollector(yesFilter);
+        let no = m.createReactionCollector(noFiler);
 
-                            yes.on("collect", v => {
-                                m.delete();
-                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.filter(r => r.presence.status !== "offline").size} Members`).then(msg => msg.delete(5000));
-                                        message.guild.members.filter(r => r.presence.status !== "offline").forEach(member => {
-                                            let bco = new Discord.RichEmbed()
-                                            .setColor("RANDOM")
-                                            .setThumbnail(message.author.avatarURL)
-                                            .setTitle("Broadcast")
-                                            .addField("Server", message.guild.name)
-                                            .addField("Sender", message.author.username)
-                                            .addField("Message", args);
+        yes.on("collect", v => {
+          m.delete();
+          message.channel
+            .send(
+              `:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${
+                message.guild.members.filter(
+                  r => r.presence.status !== "offline"
+                ).size
+              } Members`
+            )
+            .then(msg => msg.delete(5000));
+          message.guild.members
+            .filter(r => r.presence.status !== "offline")
+            .forEach(member => {
+              let bco = new Discord.RichEmbed()
+                .setColor("RANDOM")
+                .setThumbnail(message.author.avatarURL)
+                .setTitle("Broadcast")
+                .addField("Server", message.guild.name)
+                .addField("Sender", message.author.username)
+                .addField("Message", args);
 
-                                            member.sendEmbed(bco);
-                                        });
-                        });
-                        no.on("collect", v => {
-                            m.delete();
-                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000));
-                        });
-                            
-                        });
-            }
+              member.sendEmbed(bco);
+            });
+        });
+        no.on("collect", v => {
+          m.delete();
+          message.channel
+            .send("**Broadcast Canceled.**")
+            .then(msg => msg.delete(3000));
+        });
+      });
+  }
 });
 
+client.on("message", message => {
+  //clear
+  var args = message.content.substring(prefix.length).split(" ");
+  if (message.content.startsWith(prefix + "clear")) {
+    if (!message.channel.guild)
+      return message.reply("**? اسف لكن هذا الامر للسيرفرات فقط **");
+    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+      return message.reply("**?  لا يوجد لديك صلاحية لمسح الشات**");
+    var msg;
+    msg = parseInt();
 
-
-
-client.on("message", message => { //clear
-              var args = message.content.substring(prefix.length).split(" ");
-              if (message.content.startsWith(prefix + "clear")) {
-                  if(!message.channel.guild) return message.reply('**? اسف لكن هذا الامر للسيرفرات فقط **');         
-     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**?  لا يوجد لديك صلاحية لمسح الشات**');
-          var msg;
-          msg = parseInt();
-        
-        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-        message.channel.sendMessage("", {embed: {
+    message.channel
+      .fetchMessages({ limit: msg })
+      .then(messages => message.channel.bulkDelete(messages))
+      .catch(console.error);
+    message.channel
+      .sendMessage("", {
+        embed: {
           title: "``تــم مسح الشات ``",
-          color: 0x5016f3, 
-          footer: {
-            
-          }
-        }}).then(msg => {msg.delete(3000)});
-                            }
-  
-       
+          color: 0x5016f3,
+          footer: {}
+        }
+      })
+      .then(msg => {
+        msg.delete(3000);
+      });
+  }
 });
-
-
-
-
 
 console.log("==================================");
 console.log("1");
