4d72d179b14d40bada25a37ccfc685b3e2179016
Glitch (hello-express) <none>
2020-12-13 11:36:17 +0000

    🐚🍡 Checkpoint ./server.js:13423147/17572 ./config.js:13423147/7689

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ setInterval(() => {
 
 //pkg
 const { Client, RichEmbed } = require("discord.js");
-var { Util } = require("discord.js");
 const {
   TOKEN,
   prefix,
@@ -25,29 +24,7 @@ const {
   CMD1
 } = require("./config");
 const client = new Client({ disableEveryone: true });
-const ytdl = require("ytdl-core");
-const canvas = require("canvas");
-const Canvas = require("canvas");
-const convert = require("hh-mm-ss");
-const fetchVideoInfo = require("youtube-info");
-const botversion = require("./package.json").version;
-const simpleytapi = require("simple-youtube-api");
-const moment = require("moment");
-const fs = require("fs");
-const util = require("util");
-const gif = require("gif-search");
-const opus = require("node-opus");
-const ms = require("ms");
-const jimp = require("jimp");
-const { get } = require("snekfetch");
-const guild = require("guild");
-const dateFormat = require("dateformat"); //npm i dateformat
-const YouTube = require("simple-youtube-api");
-const hastebins = require("hastebin-gen");
-const pretty = require("pretty-ms");
 client.login(TOKEN);
-const queue = new Map();
-var table = require("table").table;
 const Discord = require("discord.js");
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag} By RYO!`);
