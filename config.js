e511764c66b1ea6a2570cbb10db6c8c970171c63
Glitch (hello-express) <none>
2020-12-30 06:25:11 +0000

    ⛹👜 Checkpoint ./config.js:26759070/369 ./package.json:26759070/13310

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 module.exports = {
   TOKEN: "NzI3ODExNjg3MzkwMTE4MDI5.XvxRdA.F5slX32V3-kn-Xe7aLwFP1vhs9U", //توكن بوتك
   prefix: "!", //بادئة البوت
-  CMD1: "b, // الامر حق البوت
+  CMD1: "", // الامر حق البوت
   nameproject: "bcnt01", //اسم البروجكت
   colorbc: "RANDOM", // هنا لون البرود كاست
-  reactromm: "740917840541909033", // حط ايدي الروم حق الرياكشن
-  reactro1mm: "<:nt01:727251755255070783>", // حط الرياكشن
-  reactro2mm: "<:nt01:727251755255070783>", // حط الرياكشن
+  reactromm: "", // حط ايدي الروم حق الرياكشن
+  reactro1mm: "", // حط الرياكشن
+  reactro2mm: "", // حط الرياكشن
   serverlogo: "https://cdn.discordapp.com/attachments/726648238199013420/793722739679952956/1c02e429a9496fc6.png" // حط شعار سيرفرك وبس
 };
