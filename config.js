0ede0071e959afd2846f1771560b370910e9af8c
Glitch (hello-express) <none>
2020-12-30 06:14:08 +0000

    🎩🐷 Checkpoint ./config.js:26759070/519 ./package.json:26759070/119529

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 
 module.exports = {
-  TOKEN: "", //توكن بوتك
-  prefix: "", //بادئة البوت
-  CMD1: "", // الامر حق البوت
-  nameproject: "mody-bc-1", //اسم البروجكت
+  TOKEN: "NzI3ODExNjg3MzkwMTE4MDI5.XvxRdA.F5slX32V3-kn-Xe7aLwFP1vhs9U", //توكن بوتك
+  prefix: "!", //بادئة البوت
+  CMD1: "b, // الامر حق البوت
+  nameproject: "bcnt01", //اسم البروجكت
   colorbc: "RANDOM", // هنا لون البرود كاست
-  reactromm: "", // حط ايدي الروم حق الرياكشن
-  reactro1mm: "", // حط الرياكشن
-  reactro2mm: "", // حط الرياكشن
-  serverlogo: "" // حط شعار سيرفرك وبس
+  reactromm: "740917840541909033", // حط ايدي الروم حق الرياكشن
+  reactro1mm: "<:nt01:727251755255070783>", // حط الرياكشن
+  reactro2mm: "<:nt01:727251755255070783>", // حط الرياكشن
+  serverlogo: "https://cdn.discordapp.com/attachments/726648238199013420/793722739679952956/1c02e429a9496fc6.png" // حط شعار سيرفرك وبس
 };
